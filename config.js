e946a2d01ab8be6ce6db52858099825dca18e435
Ryuichi Sai <ryuichi@rice.edu>
2020-11-29 09:56:44 -0600

    Update descriptions and redesign the UI

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var WasmExplorerVersion = "v2.18";
+var WasmExplorerVersion = "v2020.11-rice-comp517-ewab";
 
 // var WasmExplorerServiceBaseUrl = "https://wasmexplorer-service.herokuapp.com/";
 var WasmExplorerServiceBaseUrl = "http://52.87.196.236:4000/"
